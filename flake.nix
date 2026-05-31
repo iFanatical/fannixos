932c8f93d35af812ec50fa560818e84a91f4b881
laptop <laptop@fanatical>
2026-05-30 22:17:51 -0700

    Rename laptop to fanzbook-nixos.

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
 
     outputs = { self, nixpkgs, home-manager, ... }@inputs: {
 	nixosConfigurations = {
-	    laptop = nixpkgs.lib.nixosSystem {
+	    fanzbook-nixos = nixpkgs.lib.nixosSystem {
 		system = "x86_64-linux";
 		specialArgs = { inherit inputs; };
 		modules = [
