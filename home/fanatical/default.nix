990140388c89bfe7f99c878eb80d04df80d06d9d
Fanatical <theawpportunist@gmail.com>
2026-05-31 00:52:37 -0700

    Added xinitrc file to home manager.

--- home/fanatical/default.nix
+++ home/fanatical/default.nix
@@ -4,6 +4,11 @@
     home.username = "fanatical";
     home.homeDirectory = "/home/fanatical";
     home.stateVersion = "25.11";
+    
+    home.file.".xinitrc".text = ''
+    #!/usr/bin/env bash
+    feh --big-fill ~/.background.jpg 2>/dev/null || xsetroot -solid '#1e1e2e'
+    '';
 
     programs.home-manager.enable = true;
     programs.git = {
