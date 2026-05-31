9917186999611a116d0fd3152b9de9aebfa9a7b4
Fanatical <theawpportunist@gmail.com>
2026-05-31 01:10:26 -0700

    Updating dwm.nix to fix link breaks (hard-coded libraries in config.mk).

--- modules/desktop/dwm.nix
+++ modules/desktop/dwm.nix
@@ -11,6 +11,13 @@
 		    rev = "master";
 		    hash = "sha256-onsp8nDL37+Y+1vasSZFF1rhDjVxZ/bZGCwMRDalw8k=";
 		};
+		postPatch = (old.postPatch or "") + ''
+		    sed -i \
+			-e 's|^X11INC.*|X11INC = '"$(pkg-config --variable=includedir x11)"'|' \
+			-e 's|^X11LIB.*|X11LIB = '"$(pkg-config --variable=libdir x11)"'|' \
+			-e 's|^FREETYPEINC.*|FREETYPEINC = '"$(pkg-config --cflags freetype2 | sed 's/^-I//')"'|' \
+			config.mk
+		'';
 	    });
 	})
     ];
