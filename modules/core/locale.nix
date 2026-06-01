0ca0ad70345bc59a351eb910d15638f686670c59
Fanatical <theawpportunist@gmail.com>
2026-06-01 10:34:32 -0700

    Added envars in locale.nix for nvim to be the editor.

--- modules/core/locale.nix
+++ modules/core/locale.nix
@@ -4,4 +4,9 @@
     time.timeZone = "America/Phoenix";
     i18n.defaultLocale = "en_US.UTF-8";
     console.keyMap = "us";
+
+    environment.variables = {
+	EDITOR = lib.mkForce "nvim";
+	VISUAL = lib.mkForce "nvim";
+    };
 }
