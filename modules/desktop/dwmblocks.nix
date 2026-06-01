c47beb0dc0ed7e1c497644bc573e5beba6bf2f18
Fanatical <theawpportunist@gmail.com>
2026-05-31 22:51:48 -0700

    Added postPatch due to symlink issues...

--- modules/desktop/dwmblocks.nix
+++ modules/desktop/dwmblocks.nix
@@ -10,6 +10,12 @@
 		    rev = "master";
 		    hash = "sha256-P/paMpQRbsxX+g8qb7j1SjAFni426jjPHIEasmQdvCI=";
 		};
+		postPatch = (old.postPatch or "") + ''
+		sed -i \
+		    -e 's|-I''${X11INC}||g' \
+		    -e 's|-L''${X11LIB}||g' \
+		    config.mk 2>/dev/null || true
+		'';
 	    });
 	})
     ];
