ff4926dc28adf4a4b08b111ed74650fc9e499fd5
Fanatical <theawpportunist@gmail.com>
2026-05-31 22:58:19 -0700

    Updated fandwmblocks repo, need updated hash.

--- modules/desktop/dwmblocks.nix
+++ modules/desktop/dwmblocks.nix
@@ -8,7 +8,7 @@
 		    owner = "iFanatical";
 		    repo = "fandwmblocks";
 		    rev = "master";
-		    hash = "sha256-P/paMpQRbsxX+g8qb7j1SjAFni426jjPHIEasmQdvCI=";
+		    hash = "";
 		};
 		postPatch = (old.postPatch or "") + ''
 		sed -i \
