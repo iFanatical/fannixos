ac98978be9e84c1d7d7d6ef9da245e0ea51d8cc3
Fanatical <theawpportunist@gmail.com>
2026-06-01 13:27:44 -0700

    Fixing formatting in .xinitrc...again.

--- home/fanatical/default.nix
+++ home/fanatical/default.nix
@@ -71,32 +71,32 @@ in
 	    recursive = true;
 	};
 	".xinitrc".text = ''
-	    #!/usr/bin/env bash
-	    
-	    export SCRIPTS="$HOME/.local/bin"
-	    export PATH="$HOME/.local/bin:$PATH"
-	    
-	    if [ -f "$HOME/.config/user-dirs.dir" ]; then
-	        set -a
-	        . "$HOME/.config/user-dirs.dirs"
-	        set +a
-	    fi
-	    
-	    cmdemo_script eDP-1 1.2
-	    sxhkd -c "$HOME/.config/sxhkd/sxhkdrc" &
-	    picom &
-	    dunst &
-	    xsettingsd &
-	    greenclip daemon &
-	    
-	    if [ -e "$HOME/source-files/background" ]; then
-	        "$HOME/source-files/fehbg.sh"
-	    else
-	        xsetroot -solid '#1e1e2e'
-	    fi
-	    
-	    dwmblocks &
-	    exec dwm 2> ~/.dwm.log;
+#!/usr/bin/env bash
+
+export SCRIPTS="$HOME/.local/bin"
+export PATH="$HOME/.local/bin:$PATH"
+
+if [ -f "$HOME/.config/user-dirs.dir" ]; then
+    set -a
+    . "$HOME/.config/user-dirs.dirs"
+    set +a
+fi
+
+cmdemo_script eDP-1 1.2
+sxhkd -c "$HOME/.config/sxhkd/sxhkdrc" &
+picom &
+dunst &
+xsettingsd &
+greenclip daemon &
+
+if [ -e "$HOME/source-files/background" ]; then
+    "$HOME/source-files/fehbg.sh"
+else
+    xsetroot -solid '#1e1e2e'
+fi
+
+dwmblocks &
+exec dwm 2> ~/.dwm.log;
     	    '';
     };
     gtk = {
