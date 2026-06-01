ea3d3b4ae2bcc39cb570fac0bb6b41e791b67b61
Fanatical <theawpportunist@gmail.com>
2026-06-01 16:58:37 -0700

    Adding xserver repeat rate values to xserver.nix.

--- modules/desktop/xserver.nix
+++ modules/desktop/xserver.nix
@@ -1,31 +1,38 @@
 { pkgs, ... }:
 
 {
-    services.xserver = {
-	enable = true;
-	xkb = {
-	    layout = "us";
-	    options = "";
-	};
-
-	displayManager.startx.enable = true;
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "us";
+      options = "";
     };
-    services.libinput.enable = true;
-
-    services.xserver.serverFlagsSection = ''
-	Option "BlankTime" "0"
-	Option "StandbyTime" "0"
-	Option "SuspendTime" "0"
-	Option "OffTime" "0"
+    displayManager.startx.enable = true;
+    
+    serverFlagsSection = ''
+      Option "BlankTime" "0"
+      Option "StandbyTime" "0"
+      Option "SuspendTime" "0"
+      Option "OffTime" "0"
     '';
     
-    fonts.packages = with pkgs; [
-	dejavu_fonts
-	liberation_ttf
-	noto-fonts
-	noto-fonts-color-emoji
-	nerd-fonts.fira-code
-	nerd-fonts.jetbrains-mono
-	ubuntu-classic
+    inputClassSections = [
+      ''
+        Identifier "system-keyboard"
+	MatchIsKeyboard "on"
+	Option "AutoRepeat" "200 35"
+      '';
     ];
+  };
+  services.libinput.enable = true;
+
+  fonts.packages = with pkgs; [
+    dejavu_fonts
+    liberation_ttf
+    noto-fonts
+    noto-fonts-color-emoji
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    ubuntu-classic
+  ];
 }
