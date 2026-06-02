23d0f418e1b2898b0b05c2dff95d9babd29b8e0c
Fanatical <theawpportunist@gmail.com>
2026-06-01 18:30:45 -0700

    Polkit agent was crashing due to missing qt5ct libraries. Added those.

--- modules/desktop/theming.nix
+++ modules/desktop/theming.nix
@@ -3,6 +3,7 @@
 {
     environment.systemPackages = with pkgs; [
 	kdePackages.breeze-gtk
+	libsForQt5.qtstyleplugins
 	adwaita-icon-theme
 	gsettings-desktop-schemas
 	(tela-circle-icon-theme.override {
