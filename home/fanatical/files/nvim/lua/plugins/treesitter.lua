95432d781ff29a9eb0fe74e52b486c50b817cff0
Fanatical <theawpportunist@gmail.com>
2026-06-01 22:35:23 -0700

    Pinning didn't work. Updated neovim to new API configuration.

--- home/fanatical/files/nvim/lua/plugins/treesitter.lua
+++ home/fanatical/files/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,27 @@
 return {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'master',
-    build = ":TSUpdate",
+    branch = 'main',
+    build = ':TSUpdate',
+    lazy = false,
     config = function()
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            highlight = { enable = true },
-            indent = { enable = true },
-            ensure_installed = {
-                "lua",
-                "bash",
-                "nix",
-                "markdown",
-                "markdown_inline",
-            },
-            auto_install = false,
+        require('nvim-treesitter').install({
+            'lua',
+            'bash',
+            'nix',
+            'markdown',
+            'markdown_inline',
+            'vim',
+            'vimdoc',
+        })
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+                local filetype = args.match
+                local lang = vim.treesitter.language.get_lang(filetype)
+                if lang and pcall(vim.treesitter.start, args.buf, lang) then
+                    vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end
+            end,
         })
     end
 }
