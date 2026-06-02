9c76ef47d6e986532326dcab6515d7ef47d0ee03
Fanatical <theawpportunist@gmail.com>
2026-06-01 17:32:30 -0700

    services...not service in polkit.nix.

--- modules/core/polkit.nix
+++ modules/core/polkit.nix
@@ -2,7 +2,7 @@
 
 {
   security.polkit.enable = true;
-  systemd.user.service.polkit-kde-agent = {
+  systemd.user.services.polkit-kde-agent = {
     description = "polkit-kde-authentication-agent-1";
     wantedBy = [ "graphical-session.target" ];
     wants = [ "graphical-session.target" ];
