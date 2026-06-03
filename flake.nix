d5824310e2249caaaa4e4544e11c703a1453b393
Fanatical <theawpportunist@gmail.com>
2026-06-03 08:52:38 -0700

    Removing xlibre, as it seems to want to rebuild the entire world.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,6 @@
 
     inputs = {
 	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-	xlibre-overlay.url = "git+https://codeberg.org/takagemacoed/xlibre-overlay?ref=dev-for-26.05";
     };
 
     outputs = { self, nixpkgs, xlibre-overlay, ... }@inputs: {
@@ -13,8 +12,6 @@
 		specialArgs = { inherit inputs; };
 		modules = [
 		    ./hosts/fanzbook-nixos
-		    xlibre-overlay.nixosModules.overlay-xlibre-xserver
-		    xlibre-overlay.nixosModules.overlay-all-xlibre-drivers
 		];
 	    };
 	};
