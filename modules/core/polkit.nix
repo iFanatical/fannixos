79d04a0832866da48a0045468bf917178f837f68
Fanatical <theawpportunist@gmail.com>
2026-06-03 14:41:14 -0700

    Adding polkit user service.

--- modules/core/polkit.nix
+++ modules/core/polkit.nix
@@ -6,4 +6,16 @@
   environment.systemPackages = with pkgs; [
     mate-polkit
   ];
+  systemd.user.services.polkit-mate = {
+    description = "MATE polkit authentication agent";
+    wantedBy = [ "graphical-session.target" ];
+    partOf = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.mate-polkit}/libexec/polkit-mate-authenitcation-agent-1";
+      Restart = "on-failure";
+      RestartSec = 3;
+    };
+  };
 }
