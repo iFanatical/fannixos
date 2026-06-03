24f77778fb22243e27569960d435bd45afa06409
Fanatical <theawpportunist@gmail.com>
2026-06-03 08:56:16 -0700

    Removing xlibre, as it seems to want to rebuild the entire world.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 	nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-    outputs = { self, nixpkgs, xlibre-overlay, ... }@inputs: {
+    outputs = { self, nixpkgs, ... }@inputs: {
 	nixosConfigurations = {
 	    fanzbook-nixos = nixpkgs.lib.nixosSystem {
 		system = "x86_64-linux";
