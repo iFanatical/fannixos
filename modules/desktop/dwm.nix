4a7e21550633fb9df57bbbc816d27053cfe66209
Fanatical <theawpportunist@gmail.com>
2026-06-03 15:35:26 -0700

    Updating dwm hash.

--- modules/desktop/dwm.nix
+++ modules/desktop/dwm.nix
@@ -9,7 +9,7 @@
 		    owner = "iFanatical";
 		    repo = "fandwm";
 		    rev = "master";
-		    hash = "";
+		    hash = "sha256-8G1N9WRgcslnFZb8tgTnpWBAhQZDG3GZaTBQ689Qe74=";
 		};
 		
 		nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ prev.pkg-config ];
