8c3b2f303b86c8b36de0cdc9281fc7055c81fa8b
iFanatical <theawpportunist@gmail.com>
2026-06-03 21:29:47 -0700

    Updating dwm hash and fixed formatting in dwm.nix.

--- modules/desktop/dwm.nix
+++ modules/desktop/dwm.nix
@@ -1,34 +1,34 @@
 { pkgs, ... }:
 
 {
-    services.xserver.windowManager.dwm.enable = true;
-    nixpkgs.overlays = [
-	(final: prev: {
-	    dwm = prev.dwm.overrideAttrs (old: {
-		src = prev.fetchFromGitHub {
-		    owner = "iFanatical";
-		    repo = "fandwm";
-		    rev = "master";
-		    hash = "sha256-alaS8U5XiOYUGo95zfHB9pxS+yT4Q1zmgfv2putEyTA=";
-		};
-		
-		nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ prev.pkg-config ];
+  services.xserver.windowManager.dwm.enable = true;
+  nixpkgs.overlays = [
+    (final: prev: {
+      dwm = prev.dwm.overrideAttrs (old: {
+        src = prev.fetchFromGitHub {
+          owner = "iFanatical";
+	  repo = "fandwm";
+	  rev = "master";
+	  hash = "";
+	};
+	
+        nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ prev.pkg-config ];
 
-		buildInputs = (old.buildInputs or []) ++ (with prev; [
-		    libxcb
-		    xcbutil
-		    xcbutilwm
-		    libXrender
-		]);
+	buildInputs = (old.buildInputs or []) ++ (with prev; [
+	  libxcb
+	  xcbutil
+	  xcbutilwm
+	  libXrender
+	]);
 
-		postPatch = (old.postPatch or "") + ''
-		    sed -i \
-			-e 's|-I''${X11INC}||g' \
-            		-e 's|-I''${FREETYPEINC}||g' \
-            		-e 's|-L''${X11LIB}||g' \
-			config.mk
-		'';
-	    });
-	})
-    ];
+	postPatch = (old.postPatch or "") + ''
+	  sed -i \
+	    -e 's|-I''${X11INC}||g' \
+    	    -e 's|-I''${FREETYPEINC}||g' \
+    	    -e 's|-L''${X11LIB}||g' \
+	      config.mk
+	'';
+      });
+    })
+  ];
 }
